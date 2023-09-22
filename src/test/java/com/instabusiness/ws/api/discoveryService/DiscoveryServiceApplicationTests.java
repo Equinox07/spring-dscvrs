c6dcfc6922d624a4a58695bbce22dcb49c634ec1
Avalon <hopegag@gmail.com>
2023-09-22 10:33:29 +0000

    init dscs

---
+++ src/test/java/com/instabusiness/ws/api/discoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instabusiness.ws.api.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
